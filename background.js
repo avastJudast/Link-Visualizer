114f2f08d6663d611d0632e8ea777feddd9e5848
avastJudast <54156703+avastJudast@users.noreply.github.com>
2022-04-23 19:22:14 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -20,12 +20,41 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   }
   return true;
 });
-
 function getURLs() {
   var urls = [];
   for(var i = 0; i < document.links.length; i ++) {
     urls.push(document.links[i].href);
   }
-  return urls;
+  var input = [];
+  urls.sort()
+  urls.forEach((url)=>{
+    if(!input.includes(url)){
+      input.push(url)
+    }
+  });
+  var output = []
+  //now we have individual links with no repeats in sorted order
+  for(i = 0; i < input.length; i++){
+    output.push(input[i].substring(0, input[i].indexOf("/", 9)));
+  }
+  output.forEach((url)=>{
+    if(!output.includes(url)){
+      output.push(recursiveFunc(output,input))
+    }
+  });
+  return output;
 }
-
+function recursiveFunc(url,all){
+  var output = [];
+  all.forEach((input)=>{
+    if(input.inlcudes(url,0) && input.indexOf("/",url.length + 1)!=-1){
+      output.push(input.subString(0,input.indexOf("/",url.length)+1));
+    }else if(input.includes(url,0) && !(input === url)){
+      output.push(input)
+    }
+  });
+  for(var i = 0; i < output.length;i++){
+    output.splice(i,0,recursiveFunc(output[i],all))
+  }
+  return output;
+}
