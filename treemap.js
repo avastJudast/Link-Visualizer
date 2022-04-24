cf25c0c33e02df8b2f348c26ab7d2e94fbbeb8a0
Kaush <84925144+ViceCase@users.noreply.github.com>
2022-04-23 21:28:00 -0400

    Update treemap.js

--- treemap.js
+++ treemap.js
@@ -1,53 +1,103 @@
-import {
-  select,
-  json,
-  tree,
-  hierarchy,
-  linkHorizontal,
-  zoom,
-  event
-} from 'd3';
+let movieDataUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
 
-const svg = select('svg');
-const width = document.body.clientWidth;
-const height = document.body.clientHeight;
+let movieData
 
-const margin = { top: 0, right: 50, bottom: 0, left: 75};
-const innerWidth = width - margin.left - margin.right;
-const innerHeight = height - margin.top - margin.bottom;
+let canvas = d3.select('#canvas')
+let tooltip = d3.select('#tooltip')
 
-const treeLayout = tree().size([innerHeight, innerWidth]);
+let drawTreeMap = () => {
 
-const zoomG = svg
-    .attr('width', width)
-    .attr('height', height)
-  .append('g');
+    let hierarchy = d3.hierarchy(movieData, (node) => {
+        return node['children']
+    }).sum((node) => {
+        return node['value']
+    }).sort((node1, node2) => {
+        return node2['value'] - node1['value']
+    })
 
-const g = zoomG.append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
+    let createTreeMap = d3.treemap()
+                            .size([1000, 600])
+    
+    createTreeMap(hierarchy)
 
-svg.call(zoom().on('zoom', () => {
-  zoomG.attr('transform', event.transform);
-}));
+    let movieTiles = hierarchy.leaves()
+    console.log(movieTiles)
 
-json('data.json')
-  .then(data => {
-    const root = hierarchy(data);
-    const links = treeLayout(root).links();
-    const linkPathGenerator = linkHorizontal()
-      .x(d => d.y)
-      .y(d => d.x);
-  
-    g.selectAll('path').data(links)
-      .enter().append('path')
-        .attr('d', linkPathGenerator);
-  
-    g.selectAll('text').data(root.descendants())
-      .enter().append('text')
-        .attr('x', d => d.y)
-        .attr('y', d => d.x)
-        .attr('dy', '0.32em')
-        .attr('text-anchor', d => d.children ? 'middle' : 'start')
-        .attr('font-size', d => 3.25 - d.depth + 'em')
-        .text(d => d.data.data.id);
-  });
+    let block = canvas.selectAll('g')
+            .data(movieTiles)
+            .enter()
+            .append('g')
+            .attr('transform', (movie) => {
+                return 'translate(' + movie['x0'] + ', ' + movie['y0'] + ')'
+            })
+
+    block.append('rect')
+            .attr('class', 'tile')
+            .attr('fill', (movie) => {
+                let category = movie['data']['category']
+                if(category === 'Action'){
+                    return 'orange'
+                }else if(category === 'Drama'){
+                    return 'lightgreen'
+                }else if(category === 'Adventure'){
+                    return 'coral'
+                }else if(category === 'Family'){
+                    return 'lightblue'
+                }else if(category === 'Animation'){
+                    return 'pink'
+                }else if(category === 'Comedy'){
+                    return 'khaki'
+                }else if(category === 'Biography'){
+                    return 'tan'
+                }
+            }).attr('data-name', (movie) => {
+                return movie['data']['name']
+            }).attr('data-category', (movie) => {
+                return movie['data']['category']
+            })
+            .attr('data-value', (movie) => {
+                return movie['data']['value']
+            })
+            .attr('width', (movie) => {
+                return movie['x1'] - movie['x0']
+            })
+            .attr('height', (movie) => {
+                return movie['y1'] - movie['y0']
+            })
+            .on('mouseover', (movie) => {
+                tooltip.transition()
+                        .style('visibility', 'visible')
+
+                let revenue = movie['data']['value'].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+
+
+                tooltip.html(
+                    '$ ' + revenue + '<hr />' + movie['data']['name']
+                )
+
+                tooltip.attr('data-value', movie['data']['value'])
+            })
+            .on('mouseout', (movie) => {
+                tooltip.transition()
+                        .style('visibility', 'hidden')
+            })
+
+    block.append('text')
+            .text((movie) => {
+                return movie['data']['name']
+            })
+            .attr('x', 5)
+            .attr('y', 20)
+}
+
+d3.json(movieDataUrl).then(
+    (data, error) => {
+        if(error){
+            console.log(error)
+        } else {
+            movieData = data
+            console.log(movieData)
+            drawTreeMap()
+        }
+    }
+)
